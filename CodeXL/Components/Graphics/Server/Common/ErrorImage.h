c45bedb61b25c8c91458201e4fe5594a7c059471
Hosier <ahosier@amd.com>
2016-05-20 12:16:54 -0400

    Defer-Load error/empty images until needed. Not doing this can cause problems in non-pointer singleton implementations

--- CodeXL/Components/Graphics/Server/Common/ErrorImage.h
+++ CodeXL/Components/Graphics/Server/Common/ErrorImage.h
@@ -1,5 +1,5 @@
 //==============================================================================
-// Copyright (c) 2013-2015 Advanced Micro Devices, Inc. All rights reserved.
+// Copyright (c) 2013-2016 Advanced Micro Devices, Inc. All rights reserved.
 /// \author AMD Developer Tools Team
 /// \file
 /// \brief  Support for an error image that can be returned to a client when an image cannot be created
@@ -8,8 +8,6 @@
 #ifndef GPS2_ERROR_IMAGE_H
 #define GPS2_ERROR_IMAGE_H
 
-#include "Logger.h"
-#include "SharedGlobal.h"
 #include "TSingleton.h"
 
 //-----------------------------------------------------------------------------
@@ -23,113 +21,35 @@ public:
     /// Sends the current image data through the command response back to the client.
     /// \param pR The Command response
     //--------------------------------------------------------------------------
-    void Send(CommandResponse* pR)
-    {
-        PsAssert(pR != NULL);
-
-        // Check to see if we were initialized OK
-        if (m_bReady == false)
-        {
-            // send an error back to the requestor (this will also be put in the log)
-            pR->SendError("The %s was not initialized successfully\n", m_strFilename.asCharArray());
-            return;
-        }
-
-        // Send the image data
-        pR->Send(m_pImageData, m_nDataLength);
-    }
+    void Send(CommandResponse* pR);
 
     //--------------------------------------------------------------------------
     /// Sends the current image data through the command response back to the client.
     /// \param pR The Command response
     //--------------------------------------------------------------------------
-    void SendDDS(CommandResponse* pR)
-    {
-        PsAssert(pR != NULL);
-        // Send a single byte. Client will detect this and ignore it.
-        pR->Send("0", 1);
-    }
+    void SendDDS(CommandResponse* pR);
 
 protected:
 
     //--------------------------------------------------------------------------
     /// Constructor
     //--------------------------------------------------------------------------
-    ErrorImageBase()
-        :  m_pImageData(NULL),
-           m_nDataLength(0),
-           m_bReady(false)
-    {
+    ErrorImageBase();
 
-    }
+    //--------------------------------------------------------------------------
+    /// Destructor
+    //--------------------------------------------------------------------------
+    ~ErrorImageBase();
 
     //--------------------------------------------------------------------------
     /// Loads the specified file into memory.
     /// \param pFilename filename of an image to load into memory
     //--------------------------------------------------------------------------
-    void Load(const char* pFilename)
-    {
-        m_strFilename = pFilename;
-
-        // Get the server path
-        const char* strServerPath;
-        strServerPath = SG_GET_PATH(ServerPath) ;
-
-        if (!strServerPath || strlen(strServerPath)  < 1)
-        {
-            Log(logERROR, "Cannot find the server executable directory name.\n");
-            return;
-        }
-
-        // Use an gtASCIIString to easily do string stuff.
-        gtASCIIString strTmp;
-        strTmp = strServerPath;
-        strTmp.append(pFilename);
-
-        // Try to open the file we are looking for
-        FILE* f = NULL;
-        fopen_s(&f, strTmp.asCharArray(), "rb");
-
-        if (f != NULL)
-        {
-            // Get the length of the data
-            fseek(f, 0, SEEK_END);
-            long length = ftell(f);
-            m_nDataLength = (unsigned int) length;
-
-            // reset position back to beginning
-            fseek(f, 0, SEEK_SET);
-
-            // Make room for it
-            m_pImageData = (char*)(malloc(sizeof(char) * m_nDataLength));
-
-            if (m_pImageData != NULL)
-            {
-                // Read the data
-                size_t dataRead = fread(m_pImageData, sizeof(char), m_nDataLength, f);
-
-                if (dataRead != m_nDataLength)
-                {
-                    Log(logWARNING, "Insufficient data read from image; image may be corrupted.\n");
-                }
-            }
-
-            // clean up
-            fclose(f);
-        }
-        else
-        {
-            Log(logERROR, "Cannot find the %s file at %s\n", m_strFilename.asCharArray(), strTmp.asCharArray());
-            return;
-        }
-
-        // Mark ourselves as ready.
-        m_bReady = true;
-    }
-
-private:
+    void Load();
 
     gtASCIIString m_strFilename;  ///< name of the file that this error image was created with
+private:
+
     char*         m_pImageData;   ///< pointer to the data for the error image
     unsigned int  m_nDataLength;  ///< the size (number of bytes) of the error image
     bool          m_bReady;       ///< indicates that the image is loaded and ready for use
@@ -148,11 +68,8 @@ class EmptyImage : public ErrorImageBase, public TSingleton< EmptyImage >
 
 protected:
 
-    /// Constructor loads the desired image
-    EmptyImage()
-    {
-        Load("Images/EmptyImage.png");
-    }
+    /// Constructor
+    EmptyImage();
 };
 
 //-----------------------------------------------------------------------------
@@ -168,11 +85,8 @@ class ErrorImage : public ErrorImageBase, public TSingleton< ErrorImage >
 
 protected:
 
-    /// Constructor loads the desired image
-    ErrorImage()
-    {
-        Load("Images/ErrorImage.png");
-    }
+    /// Constructor
+    ErrorImage();
 };
 
 #endif // GPS2_ERROR_IMAGE_H
