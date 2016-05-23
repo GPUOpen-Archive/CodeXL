dcf04f1578daefba959209e53a91f596f82e6ff1
salgrana <Sigal.Algranaty@amd.com>
2016-05-23 12:37:21 +0300

    Fixed the link click in the summary tables

--- CodeXL/Components/GpuProfiling/AMDTGpuProfiling/gpTraceView.cpp
+++ CodeXL/Components/GpuProfiling/AMDTGpuProfiling/gpTraceView.cpp
@@ -749,7 +749,7 @@ void gpTraceView::SelectItemInTraceTables(ProfileSessionDataItem* pItem, bool se
                 for (int i = 0; i < m_pGPUTraceTablesTabWidget->count(); i++)
                 {
                     QString tabText = m_pGPUTraceTablesTabWidget->tabText(i);
-                    QString queueDisplayName = ProfileSessionDataItem::QueueDisplayName(pItem->QueueName());
+                    QString queueDisplayName = m_pSessionDataContainer->QueueNameFromPointer(pItem->QueueName());
 
                     if (tabText.contains(queueDisplayName))
                     {
