efd9dcfc15238c75032d0ea2df9cb626dd1ff305
Roman-Bober <roman.bober@amd.com>
2016-06-15 10:07:30 +0300

    bug fix CODEXL-3148, use in windows AGS lib for querying Radeon software version

--- Common/Src/AMDTBaseProjectsTests/src/AGSLib_test.cpp
+++ Common/Src/AMDTBaseProjectsTests/src/AGSLib_test.cpp
@@ -2,16 +2,18 @@
 #pragma warning(disable : 4100)
 
 
+#include <iostream>
 #include <gtest/gtest.h>
 #include <inc/amd_ags.h>
 
+using namespace std;
 TEST(agsLib, GetVersion)
 {
     AGSContext* agsContext = nullptr;
-    
     AGSGPUInfo gpuInfo;
     AGSReturnCode rc = agsInit(&agsContext, nullptr, &gpuInfo);
     EXPECT_EQ(rc, AGS_SUCCESS);
+    cout << "Radeon version " << gpuInfo.radeonSoftwareVersion << endl;
     rc = agsDeInit(agsContext);
     agsContext = nullptr;
     EXPECT_EQ(rc, AGS_SUCCESS);
