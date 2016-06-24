2d3792b308f1d0bf531915e77e7bd95e4f07d4f7
Rodrigo Urra <Rodrigo.Urra@amd.com>
2016-06-24 00:52:22 -0400

    Move a Vulkan entry point from GDPA to GIPA (vkGetPhysicalDeviceSparseImageFormatProperties)

--- CodeXL/Components/Graphics/Server/VulkanServer/VKT/Interception/vktIntercept.cpp
+++ CodeXL/Components/Graphics/Server/VulkanServer/VKT/Interception/vktIntercept.cpp
@@ -3529,8 +3529,6 @@ VK_LAYER_EXPORT VKAPI_ATTR PFN_vkVoidFunction VKAPI_CALL vkGetDeviceProcAddr(VkD
         return (PFN_vkVoidFunction)Mine_vkGetDeviceMemoryCommitment;
     if (!strcmp(funcName, "vkGetImageSparseMemoryRequirements"))
         return (PFN_vkVoidFunction)Mine_vkGetImageSparseMemoryRequirements;
-    if (!strcmp(funcName, "vkGetPhysicalDeviceSparseImageFormatProperties"))
-        return (PFN_vkVoidFunction)Mine_vkGetPhysicalDeviceSparseImageFormatProperties;
     if (!strcmp(funcName, "vkGetImageMemoryRequirements"))
         return (PFN_vkVoidFunction)Mine_vkGetImageMemoryRequirements;
     if (!strcmp(funcName, "vkGetBufferMemoryRequirements"))
@@ -3662,6 +3660,8 @@ VK_LAYER_EXPORT VKAPI_ATTR PFN_vkVoidFunction VKAPI_CALL vkGetInstanceProcAddr(V
         return (PFN_vkVoidFunction)Mine_vkGetPhysicalDeviceFeatures;
     if (!strcmp(funcName, "vkGetPhysicalDeviceFormatProperties"))
         return (PFN_vkVoidFunction)Mine_vkGetPhysicalDeviceFormatProperties;
+    if (!strcmp(funcName, "vkGetPhysicalDeviceSparseImageFormatProperties"))
+        return (PFN_vkVoidFunction)Mine_vkGetPhysicalDeviceSparseImageFormatProperties;
     if (!strcmp(funcName, "vkEnumerateInstanceLayerProperties"))
         return (PFN_vkVoidFunction)Mine_vkEnumerateInstanceLayerProperties;
     if (!strcmp(funcName, "vkEnumerateInstanceExtensionProperties"))
