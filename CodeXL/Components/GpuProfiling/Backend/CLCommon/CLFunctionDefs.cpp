GPUOpen-Archive/CodeXL

commit da899989d75f018a9727ea86021d4d2ac8a75dbb

change  X64 macro into AMDT_ADDRESS_SPACE_TYPE == AMDT_64_BIT_ADDRESS_SPACE

CodeXL/Components/GpuProfiling/Backend/CLCommon/CLFunctionDefs.cpp
@@ -24,7 +24,7 @@ void InitNextCLFunctions(cl_icd_dispatch_table& table)
 }
 
 #ifdef _WIN32
-    #ifdef X64
+   #if AMDT_ADDRESS_SPACE_TYPE == AMDT_64_BIT_ADDRESS_SPACE
         #define AMDOCL_MODULE_NAME "amdocl64.dll"
     #else
         #define AMDOCL_MODULE_NAME "amdocl.dll"

CodeXL/Components/GpuProfiling/Backend/Common/Defs.h
@@ -27,7 +27,7 @@ using namespace GPULogger;
 // @{
 
 #ifdef WIN32
-    #ifdef X64
+   #if AMDT_ADDRESS_SPACE_TYPE == AMDT_64_BIT_ADDRESS_SPACE
         #define BITNESS "-x64"
         #define NUMBITS "64"
     #else
@@ -67,7 +67,7 @@ using namespace GPULogger;
 #define HSA_PROFILE_AGENT_DLL LIB_PREFIX GPU_PROFILER_LIBRARY_NAME_PREFIX "HSAProfileAgent" BITNESS DEBUG_PREFIX GDT_BUILD_SUFFIX LIB_SUFFIX
 
 #ifdef WIN32
-    #ifdef X64
+   #if AMDT_ADDRESS_SPACE_TYPE == AMDT_64_BIT_ADDRESS_SPACE
         #define HSA_RUNTIME_TOOLS_LIB "hsa-runtime-tools64.dll"
     #else
         #define HSA_RUNTIME_TOOLS_LIB "hsa-runtime-tools.dll"

CodeXL/Components/Graphics/Server/Common/Communication_Impl.cpp
@@ -1402,7 +1402,7 @@ bool SendFileResponse(CommunicationID& requestID, const char* cpFile, NetSocket*
 
     // collect file data and generate response
     FILE* in;
-#if defined WIN32 && !defined X64
+#if defined WIN32 &&  (AMDT_ADDRESS_SPACE_TYPE == AMDT_32_BIT_ADDRESS_SPACE)
     long fileSize, bytesRead;
 #else
     size_t fileSize, bytesRead;

CodeXL/Components/Graphics/Server/Common/NamedEvent.cpp
@@ -281,7 +281,7 @@ static const int BUFFER_SIZE = 16;  ///< size of shared memory buffer, in bytes
 // (with shared memory), 32 and 64-bit versions need to be unique, since the
 // shared memory consists of a data structure whose size is dependent on
 // the bitsize.
-#if defined X64
+#if AMDT_ADDRESS_SPACE_TYPE == AMDT_64_BIT_ADDRESS_SPACE
     static const char* EXT = "_x64";
 #else
     static const char* EXT = "_x86";

CodeXL/Components/Graphics/Server/Common/NamedMutex.cpp
@@ -208,7 +208,7 @@ private:
 // (with shared memory), 32 and 64-bit versions need to be unique, since the
 // shared memory consists of a data structure whose size is dependent on
 // the bitsize.
-#if defined X64
+#if AMDT_ADDRESS_SPACE_TYPE == AMDT_64_BIT_ADDRESS_SPACE
     static const char* EXT = "_x64";
 #else
     static const char* EXT = "_x86";

CodeXL/Components/Graphics/Server/Common/NamedSemaphore.cpp
@@ -218,7 +218,7 @@ static const int BUFFER_SIZE = 16;  ///< size of shared memory buffer
 // (with shared memory), 32 and 64-bit versions need to be unique, since the
 // shared memory consists of a data structure whose size is dependent on
 // the bitsize.
-#if defined X64
+#if AMDT_ADDRESS_SPACE_TYPE == AMDT_64_BIT_ADDRESS_SPACE
     static const char* EXT = "_x64";
 #else
     static const char* EXT = "_x86";

CodeXL/Components/Graphics/Server/Common/xml.cpp
@@ -261,7 +261,7 @@ gtASCIIString XMLHexPtr(const char* strNode, const void* ptr)
     // 32 or 64 bit hex value returned
     if (ptr == NULL)
     {
-#ifdef X64
+#if AMDT_ADDRESS_SPACE_TYPE == AMDT_64_BIT_ADDRESS_SPACE
         text = "0x000000000000000%d";
 #else
         text = "0x0000000%d";

CodeXL/Components/Graphics/Server/DXGIServer/ALVRGPUAffinityWrapper.h
@@ -32,7 +32,7 @@ bool GPUCount(ID3D11Device* pDevice, UINT* nCount)
 {
     HMODULE hDll = NULL;
 
-#ifdef X64
+#if AMDT_ADDRESS_SPACE_TYPE == AMDT_64_BIT_ADDRESS_SPACE
     hDll = GetModuleHandle("atidxx64.dll");
 #else
     hDll = GetModuleHandle("atidxx32.dll");
@@ -40,7 +40,7 @@ bool GPUCount(ID3D11Device* pDevice, UINT* nCount)
 
     if (hDll == NULL)
     {
-#ifdef X64
+#if AMDT_ADDRESS_SPACE_TYPE == AMDT_64_BIT_ADDRESS_SPACE
         Log(logERROR, "Unable to initialize because 'atidxx64.dll' is not available.");
 #else
         Log(logERROR, "Unable to initialize because 'atidxx32.dll' is not available.");

commit e9e001bfef5e81605b116dbb0a9919a78d28caaa

Fix regression introduced by da899989d75f018a9727ea86021d4d2ac8a75dbb -- 32 bit GPU Profiling was completely broken.  See CODEXL-3267

CodeXL/Components/GpuProfiling/Backend/CLCommon/CLFunctionDefs.cpp
@@ -7,9 +7,10 @@
 
 #include <cstring>
 
+#include <AMDTBaseTools/Include/AMDTDefinitions.h>
+
 #include "CLFunctionDefs.h"
 #include "OpenCLModule.h"
-#include <cstring>
 
 cl_icd_dispatch_table g_nextDispatchTable;
 cl_icd_dispatch_table g_realDispatchTable;