8d76e7f0ea413835aa5ba7085e1ca50a0c3a5e9b
Callan McInally <Callan.McInally@amd.com>
2016-08-29 10:37:58 -0400

    factor Common/Lib/Ext/Qt into submodule

--- Common/Lib/Ext/Qt/5.5/Build/CentOS66/x86_64/release/lib/cmake/Qt5Location/Qt5Location_QGeoServiceProviderFactoryMapbox.cmake
+++
@@ -1,6 +0,0 @@
-
-add_library(Qt5::QGeoServiceProviderFactoryMapbox MODULE IMPORTED)
-
-_populate_Location_plugin_properties(QGeoServiceProviderFactoryMapbox RELEASE "geoservices/libqtgeoservices_mapbox.so")
-
-list(APPEND Qt5Location_PLUGINS Qt5::QGeoServiceProviderFactoryMapbox)
--- Common/Lib/Ext/Qt/5.5/Build/CentOS66/x86_64/release/lib/cmake/Qt5Location/Qt5Location_QGeoServiceProviderFactoryOsm.cmake
+++
@@ -1,6 +0,0 @@
-
-add_library(Qt5::QGeoServiceProviderFactoryOsm MODULE IMPORTED)
-
-_populate_Location_plugin_properties(QGeoServiceProviderFactoryOsm RELEASE "geoservices/libqtgeoservices_osm.so")
-
-list(APPEND Qt5Location_PLUGINS Qt5::QGeoServiceProviderFactoryOsm)
