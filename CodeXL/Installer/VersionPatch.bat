69277c5c14d73e5077efd9a6256780dc939e3136
Callan McInally <Callan.McInally@amd.com>
2016-12-15 16:20:24 -0500

    More AMDT updates

--- CodeXL/Installer/VersionPatch.bat
+++ CodeXL/Installer/VersionPatch.bat
@@ -6,7 +6,7 @@ rem Call the script that patches the file version and product version informatio
 rem Arg1 = Full path of the PowerShell script file
 rem Arg2 = Full path for workspace root folder
 rem Arg3 = Product Version 
-rem Arg4 = GDT_Suffix (Public or NDA or Internal)
+rem Arg4 = AMDT_Suffix (Public or NDA or Internal)
 rem Arg5  = (Optional) "-debug True" - will output detailed information. 
 
-Call Powershell -ExecutionPolicy Unrestricted -command %1 -workspace %2 -VersionNumber %3 -GDT_Build_Suffix %4
+Call Powershell -ExecutionPolicy Unrestricted -command %1 -workspace %2 -VersionNumber %3 -AMDT_Build_Suffix %4
