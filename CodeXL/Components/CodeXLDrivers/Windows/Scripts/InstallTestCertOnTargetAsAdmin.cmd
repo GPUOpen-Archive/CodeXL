50d9340b7285722193f3af27b495ea87597ce46e
gnanabaskaran <gnanabaskaran.muthumani@amd.com>
2016-12-19 14:39:10 +0530

    Remove the old scripts that are not required.

--- CodeXL/Components/CodeXLDrivers/Windows/Scripts/InstallTestCertOnTargetAsAdmin.cmd
+++
@@ -1,3 +0,0 @@
-IF NOT EXIST UnitTesting.cer makecert -$ individual -r -pe -ss "CpuProf Test Store" -n CN="CpuProf Unit Test" UnitTesting.cer
-certmgr.exe -add UnitTesting.cer -s -r localMachine root
-certmgr.exe -add UnitTesting.cer -s -r localMachine trustedpublisher
