38f1a9ae7648e52f76cc2c225cd9fc9039e961e1
Aalok Agarwal <aalok.agarwal@amd.com>
2017-02-15 15:17:20 +0530

    Creating linux driver tar file during compilation of source code.

--- CodeXL/Components/PowerProfiling/Backend/AMDTPowerProfilingDrivers/Linux/createPwrProfSourcePkg.sh
+++ CodeXL/Components/PowerProfiling/Backend/AMDTPowerProfilingDrivers/Linux/createPwrProfSourcePkg.sh
@@ -7,11 +7,16 @@
 
 # save the current directory path 
 MODULE_SOURCE=`pwd`
+DRV_SRC="$MODULE_SOURCE/Components/PowerProfiling/Backend/AMDTPowerProfilingDrivers/Linux/"
+COMMON_INC_DIR="$MODULE_SOURCE/Components/PowerProfiling/Backend/AMDTPowerProfilingDrivers/inc/"
+COMMON_SRC_DIR="$MODULE_SOURCE/Components/PowerProfiling/Backend/AMDTPowerProfilingDrivers/common/"
+LINUX_INC_DIR="$MODULE_SOURCE/Components/PowerProfiling/Backend/AMDTPowerProfilingDrivers/Linux/inc/"
+LINUX_SRC_DIR="$MODULE_SOURCE/Components/PowerProfiling/Backend/AMDTPowerProfilingDrivers/Linux/src/"
 
 # Module Name
 MODULE_NAME=amdtPwrProf
 # read Module version from ./VERSION file
-MODULE_VERSION=$(cat CodeXLPwrProfVersion)
+MODULE_VERSION=$(cat $DRV_SRC/CodeXLPwrProfVersion)
 # create a tempaary directory in /tmp/
 SOURCE_DIR=/tmp/$MODULE_NAME-$MODULE_VERSION
 
@@ -22,24 +27,19 @@ mkdir -p $SOURCE_DIR/inc
 mkdir -p $SOURCE_DIR/inc/Smu8Header
 
 # copy
-cp -r ../inc/AMDT*.h $SOURCE_DIR/inc/
-cp -r ../inc/Smu8Header/AMDT* $SOURCE_DIR/inc/Smu8Header/
-cp -r inc/AMDT*.h $SOURCE_DIR/inc/
-cp -r ../common/AMDT*.c $SOURCE_DIR/src/
-cp -r src/AMDT*.c $SOURCE_DIR/src/
-cp Makefile $SOURCE_DIR
-cp dkms.conf $SOURCE_DIR
-cp CodeXLPwrProfVersion $SOURCE_DIR
+cp -r $COMMON_INC_DIR/AMDT*.h $SOURCE_DIR/inc/
+cp -r $COMMON_INC_DIR/Smu8Header/AMDT* $SOURCE_DIR/inc/Smu8Header/
+cp -r $LINUX_INC_DIR/AMDT*.h $SOURCE_DIR/inc/
+cp -r $COMMON_SRC_DIR/AMDT*.c $SOURCE_DIR/src/
+cp -r $LINUX_SRC_DIR/AMDT*.c $SOURCE_DIR/src/
 
-# remove debug files
-rm -f src/AMDTPwrProfDebugHelper.c 2> /dev/null
-rm -f inc/AMDTPwrProfDebugHelper.h 2> /dev/null
+cp $DRV_SRC/Makefile $SOURCE_DIR
+cp $DRV_SRC/dkms.conf $SOURCE_DIR
+cp $DRV_SRC/CodeXLPwrProfVersion $SOURCE_DIR
 
 # tar the files in current directory
-cd /tmp/
-tar -zcf $MODULE_SOURCE/CodeXLPwrProfDriverSource.tar.gz $MODULE_NAME-$MODULE_VERSION/
+tar -C /tmp/ -zcf /tmp/CodeXLPwrProfDriverSource.tar.gz $MODULE_NAME-$MODULE_VERSION/
 echo "CodeXLPwrProfDriverSource.tar.gz created."
-cd $MODULE_SOURCE
 
 # after taring delete the temp file.
 rm -rf $SOURCE_DIR 
