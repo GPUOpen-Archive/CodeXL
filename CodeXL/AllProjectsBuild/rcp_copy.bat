7f920f1ab6c1afc80ce6456a1b0637976048a386
Chris Hesik <christopher.hesik@amd.com>
2017-04-28 00:44:51 -0400

    Fix paths in rcp_copy and make sure rcp_copy.bat is called after building AMDTGpuProfiling

--- CodeXL/AllProjectsBuild/rcp_copy.bat
+++ CodeXL/AllProjectsBuild/rcp_copy.bat
@@ -2,6 +2,6 @@
 
 set CONFIG_NAME=%1
 
-XCopy /s /i "..\..\Common\Lib\AMD\RCP\RadeonComputeProfiler\bin\*.*" "..\Output\%CONFIG_NAME%\bin\" /y
-XCopy /s /i "..\..\Common\Lib\AMD\RCP\RadeonComputeProfiler\jqPlot\*.*" "..\Output\%CONFIG_NAME%\bin\jqPlot\" /y
-XCopy /s /i "..\..\Common\Lib\AMD\RCP\RCPProfileDataParser\bin\*.*" "..\Output\%CONFIG_NAME%\bin\" /y
+XCopy /s /i "..\..\..\..\Common\Lib\AMD\RCP\RadeonComputeProfiler\bin\*.*" "..\..\..\Output\%CONFIG_NAME%\bin\" /y
+XCopy /s /i "..\..\..\..\Common\Lib\AMD\RCP\RadeonComputeProfiler\jqPlot\*.*" "..\..\..\Output\%CONFIG_NAME%\bin\jqPlot\" /y
+XCopy /s /i "..\..\..\..\Common\Lib\AMD\RCP\RCPProfileDataParser\bin\*.*" "..\..\..\Output\%CONFIG_NAME%\bin\" /y
