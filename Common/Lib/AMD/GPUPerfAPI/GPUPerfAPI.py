fd44dc6c8fe34c2c7955b5b89db9f08ea7648373
Chris Hesik <christopher.hesik@amd.com>
2017-04-28 21:44:45 -0400

    adapt to new GPUPerfAPI dir layout

--- Common/Lib/AMD/GPUPerfAPI/GPUPerfAPI.py
+++ Common/Lib/AMD/GPUPerfAPI/GPUPerfAPI.py
@@ -5,12 +5,4 @@ INCLUDE_TREE = {
         'debug-x86_64'      : 'Include/',
         'release-x86_64'    : 'Include/'
         }
-LIBRARY_TREE = {
-        'debug-x86'        : 'Bin/Linx86',
-        'release-x86'      : 'Bin/Linx86',
-        'debug-x86_64'     : 'Bin/Linx64',
-        'release-x86_64'   : 'Bin/Linx64',
-        }
 LIBS = []
-
-COPY_LIBS = ['libGPUPerfAPICounters.so']
