581affde378486e4bfac7af511a1cd5254f5f8db
rajeebbarman <rajeeb.barman@amd.com>
2017-10-27 12:07:20 +0530

    Build fix for platform id check.

--- CodeXL/Components/PowerProfiling/Backend/AMDTPowerProfileAPI/src/AMDTPowerProfileControl.cpp
+++ CodeXL/Components/PowerProfiling/Backend/AMDTPowerProfileAPI/src/AMDTPowerProfileControl.cpp
@@ -851,7 +851,7 @@ AMDTResult AMDTPwrGetTargetSystemInfo(
             }
 
             if ((PLATFORM_ZEPPELIN != g_pTargetSystemInfo->m_platformId)
-                && (PLATFORM_INVALID != g_pTargetSystemInfo->m_targetInfo.m_platformId))
+                && (PLATFORM_INVALID != g_pTargetSystemInfo->m_platformId))
             {
                 // Fill the pState frequency table
                 PrepareApuPstateTable();
